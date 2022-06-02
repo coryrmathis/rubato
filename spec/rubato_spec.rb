fdd9614d523202013f3a4bbcc394edee0b6bbb19
Cory Mathis <corymathis@Corys-MBP.lan1>
2022-06-02 12:32:42 -0500

    Make tests pass.

--- spec/rubato_spec.rb
+++ spec/rubato_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rubato do
   it "has a version number" do
     expect(Rubato::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
